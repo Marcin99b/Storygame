4a23046999d0ea4a3abadfff19ab93f13d0202bd
Marcin99b <marcin.bigos99@gmail.com>
2026-04-13 22:41:16 +0200

    User object

---
+++ src/Storygame.Users/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Storygame.Users;
+
+public class User
+{
+    /// <summary>
+    /// User object cannot be older than User class definition
+    /// </summary>
+    private static readonly DateTime MIN_VERIFICATION_DATE_TIME = new DateTime(2026, 4, 13);
+
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required DateTime RegisteredAt { get; set; }
+    public required DateTime? VerifiedAt { get; set; }
+    public bool IsVerified => VerifiedAt.GetValueOrDefault() > MIN_VERIFICATION_DATE_TIME;
+}
