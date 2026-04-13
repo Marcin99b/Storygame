3cc48be40327c8f4c7c9088ed420eb60cf637818
Marcin99b <marcin.bigos99@gmail.com>
2026-04-13 22:31:18 +0200

    add tracking

---
+++ src/Storygame.Tracking/Tracking.cs
@@ -0,0 +1,11 @@
+﻿namespace Storygame.Tracking;
+
+public class Tracking
+{
+    public required Guid Id { get; set; }
+    public required Guid LibraryBookId { get; set; }
+    public required int TotalLength { get; set; }
+    public required int CurrentIndex { get; set; }
+    public bool IsStarted => CurrentIndex > 0;
+    public bool IsFinished => CurrentIndex == TotalLength;
+}
