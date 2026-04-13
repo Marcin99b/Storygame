22e1cd438df79a91938910fe51c4a631a8fd709e
Marcin99b <marcin.bigos99@gmail.com>
2026-04-13 22:32:00 +0200

    add user id

--- src/Storygame.Tracking/Tracking.cs
+++ src/Storygame.Tracking/Tracking.cs
@@ -4,6 +4,7 @@ public class Tracking
 {
     public required Guid Id { get; set; }
     public required Guid LibraryBookId { get; set; }
+    public required Guid UserId { get; set; }
     public required int TotalLength { get; set; }
     public required int CurrentIndex { get; set; }
     public bool IsStarted => CurrentIndex > 0;
