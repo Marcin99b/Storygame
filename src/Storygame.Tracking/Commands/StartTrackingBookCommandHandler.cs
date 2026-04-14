fecf4c266977ca224b84478bc8c6efafd07a9555
Marcin99b <marcin.bigos99@gmail.com>
2026-04-14 11:18:00 +0200

    StartTrackingBookCommandHandler

---
+++ src/Storygame.Tracking/Commands/StartTrackingBookCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Storygame.Cqrs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Tracking.Commands;
+
+public record StartTrackingBookCommand : ICommand;
+
+public class StartTrackingBookCommandHandler : ICommandHandler<StartTrackingBookCommand>
+{
+    public Task HandleAsync(StartTrackingBookCommand command) => throw new NotImplementedException();
+}
