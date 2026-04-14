ba1925d451fdbdc1ef6110c6568742db0e8e075c
Marcin99b <marcin.bigos99@gmail.com>
2026-04-14 11:45:19 +0200

    AddBookToLibraryCommandHandler

--- src/Storygame.Library/Commands/AddBookToLibraryCommandHandler.cs
+++ src/Storygame.Library/Commands/AddBookToLibraryCommandHandler.cs
@@ -5,9 +5,32 @@ using System.Text;
 
 namespace Storygame.Library.Commands;
 
-public record AddBookToLibraryCommand() : ICommand;
+public record AddBookToLibraryCommand(Guid UserId, Guid? CatalogBookId, Guid? ImageId, string Title, string Description, MediaType MediaType, int Length) : ICommand;
 
-public class AddBookToLibraryCommandHandler : ICommandHandler<AddBookToLibraryCommand>
+public class AddBookToLibraryCommandHandler(ILibraryRepository libraryRepository) : ICommandHandler<AddBookToLibraryCommand>
 {
-    public Task HandleAsync(AddBookToLibraryCommand command) => throw new NotImplementedException();
+    public async Task HandleAsync(AddBookToLibraryCommand command)
+    {
+        var alreadyExist = command.CatalogBookId.HasValue 
+            && await libraryRepository.CheckIfUserAlreadyHasThisBook(command.CatalogBookId.Value, command.MediaType);
+        if (alreadyExist)
+        {
+            throw new ArgumentException($"User already has book: {command.CatalogBookId!.Value} with media type: {command.MediaType.ToString()}");
+        }
+
+        var book = new Book()
+        {
+            Id = Guid.NewGuid(),
+            UserId = command.UserId,
+            CatalogBookId = command.CatalogBookId,
+            ImageId = command.ImageId,
+            Title = command.Title,
+            Description = command.Description,
+            Length = command.Length,
+            MediaType = command.MediaType,
+            AddedToLibraryAt = DateTime.UtcNow
+        };
+
+        libraryRepository.AddBook(book);
+    }
 }
