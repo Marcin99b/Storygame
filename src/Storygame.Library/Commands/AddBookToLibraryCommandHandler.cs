10e94f7803d65e202fe87c8b065947dbaf2deb9e
Marcin99b <marcin.bigos99@gmail.com>
2026-04-14 11:14:39 +0200

    add queries and handlers

---
+++ src/Storygame.Library/Commands/AddBookToLibraryCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Storygame.Cqrs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Library.Commands;
+
+public record AddBookToLibraryCommand() : ICommand;
+
+public class AddBookToLibraryCommandHandler : ICommandHandler<AddBookToLibraryCommand>
+{
+    public Task HandleAsync(AddBookToLibraryCommand command) => throw new NotImplementedException();
+}
---
+++ src/Storygame.Library/Queries/GetUserBooksFromLibraryQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Storygame.Cqrs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Library.Queries;
+
+public record GetUserBooksFromLibraryQuery : IQuery<GetUserBooksFromLibraryQueryResult>;
+public record GetUserBooksFromLibraryQueryResult;
+
+public class GetUserBooksFromLibraryQueryHandler : IQueryHandler<GetUserBooksFromLibraryQuery, GetUserBooksFromLibraryQueryResult>
+{
+    public Task<GetUserBooksFromLibraryQueryResult> HandleAsync(GetUserBooksFromLibraryQuery query)
+    {
+        return Task.FromResult(new GetUserBooksFromLibraryQueryResult());
+    }
+}
