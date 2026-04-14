2f3e6c77eefc93c6b13d0f8de3f124699d4d9731
Marcin99b <marcin.bigos99@gmail.com>
2026-04-14 11:52:16 +0200

    ITrackingRepository

---
+++ src/Storygame.Tracking/ITrackingRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Tracking;
+
+public interface ITrackingRepository
+{
+    void AddTracking(Tracking tracking);
+    Task<bool> CheckIfBookIsAlreadyTracked(Guid libraryBookId);
+}
