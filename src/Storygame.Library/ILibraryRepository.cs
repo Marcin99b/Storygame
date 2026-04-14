d7011c541ecec16c07209558c53426cad1575005
Marcin99b <marcin.bigos99@gmail.com>
2026-04-14 11:38:20 +0200

    ILibraryRepository

---
+++ src/Storygame.Library/ILibraryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Library;
+
+public interface ILibraryRepository
+{
+    public void AddBook(Book book);
+    public Task<bool> CheckIfUserAlreadyHasThisBook(Guid catalogBookId, MediaType mediaType);
+    public Task<IEnumerable<Book>> GetUserBooks(Guid userId);
+}
--- src/Storygame.Library/Queries/GetUserBooksFromLibraryQueryHandler.cs
+++ src/Storygame.Library/Queries/GetUserBooksFromLibraryQueryHandler.cs
@@ -5,13 +5,14 @@ using System.Text;
 
 namespace Storygame.Library.Queries;
 
-public record GetUserBooksFromLibraryQuery : IQuery<GetUserBooksFromLibraryQueryResult>;
+public record GetUserBooksFromLibraryQuery(Guid UserId) : IQuery<GetUserBooksFromLibraryQueryResult>;
 public record GetUserBooksFromLibraryQueryResult;
 
-public class GetUserBooksFromLibraryQueryHandler : IQueryHandler<GetUserBooksFromLibraryQuery, GetUserBooksFromLibraryQueryResult>
+public class GetUserBooksFromLibraryQueryHandler(ILibraryRepository libraryRepository) : IQueryHandler<GetUserBooksFromLibraryQuery, GetUserBooksFromLibraryQueryResult>
 {
-    public Task<GetUserBooksFromLibraryQueryResult> HandleAsync(GetUserBooksFromLibraryQuery query)
+    public async Task<GetUserBooksFromLibraryQueryResult> HandleAsync(GetUserBooksFromLibraryQuery query)
     {
-        return Task.FromResult(new GetUserBooksFromLibraryQueryResult());
+        var books = libraryRepository.GetUserBooks(query.UserId);
+        return new GetUserBooksFromLibraryQueryResult();
     }
 }
