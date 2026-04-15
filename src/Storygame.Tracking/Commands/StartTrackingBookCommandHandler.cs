9e17a09c67bfb43a04dea3e6584b8ab8fcdfea66
Marcin99b <marcin.bigos99@gmail.com>
2026-04-15 10:26:26 +0200

    StartTrackingBookCommandHandler

--- src/Storygame.Tracking/Commands/StartTrackingBookCommandHandler.cs
+++ src/Storygame.Tracking/Commands/StartTrackingBookCommandHandler.cs
@@ -5,9 +5,26 @@ using System.Text;
 
 namespace Storygame.Tracking.Commands;
 
-public record StartTrackingBookCommand : ICommand;
+public record StartTrackingBookCommand(Guid LibraryBookId, Guid UserId, int TotalLength) : ICommand;
 
-public class StartTrackingBookCommandHandler : ICommandHandler<StartTrackingBookCommand>
+public class StartTrackingBookCommandHandler(ITrackingRepository trackingRepository) : ICommandHandler<StartTrackingBookCommand>
 {
-    public Task HandleAsync(StartTrackingBookCommand command) => throw new NotImplementedException();
+    public async Task HandleAsync(StartTrackingBookCommand command)
+    {
+        if (await trackingRepository.CheckIfBookIsAlreadyTracked(command.LibraryBookId))
+        {
+            throw new ArgumentException($"Book {command.LibraryBookId} is already tracked");
+        }
+
+        var tracking = new Tracking()
+        {
+            Id = Guid.NewGuid(),
+            UserId = command.UserId,
+            CurrentIndex = 0,
+            LibraryBookId = command.LibraryBookId,
+            TotalLength = command.TotalLength
+        };
+
+        trackingRepository.AddTracking(tracking);
+    }
 }
