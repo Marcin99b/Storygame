7cecb88192496813bdb3d7e9a00cc6310b4a1545
Marcin99b <marcin.bigos99@gmail.com>
2026-04-15 16:34:38 +0200

    add mongo

---
+++ src/Storygame.Storage/LibraryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Storygame.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Storage;
+
+public class LibraryRepository : ILibraryRepository
+{
+    public void AddBook(Book book)
+    {
+    }
+
+    public Task<bool> CheckIfUserAlreadyHasThisBook(Guid catalogBookId, MediaType mediaType) => throw new NotImplementedException();
+    public Task<IEnumerable<Book>> GetUserBooks(Guid userId) => throw new NotImplementedException();
+}
---
+++ src/Storygame.Storage/TrackingRepository.cs
@@ -0,0 +1,9 @@
+﻿using Storygame.Tracking;
+
+namespace Storygame.Storage;
+
+public class TrackingRepository : ITrackingRepository
+{
+    public void AddTracking(Tracking.Tracking tracking) => throw new NotImplementedException();
+    public Task<bool> CheckIfBookIsAlreadyTracked(Guid libraryBookId) => throw new NotImplementedException();
+}
