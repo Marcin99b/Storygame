435195558819aa17e4d41b4055558b21a189e8bf
Marcin99b <marcin.bigos99@gmail.com>
2026-04-16 05:58:07 +0200

    healthcheck

--- src/Storygame.Web/Program.cs
+++ src/Storygame.Web/Program.cs
@@ -1,11 +1,12 @@
-using Storygame.Cqrs;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Driver;
+using Storygame.Cqrs;
 using Storygame.Storage;
 using Storygame.Web.Areas.Catalog;
 using Storygame.Web.Areas.Library;
 using Storygame.Web.Areas.Tracking;
 using Storygame.Web.Areas.Users;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,7 +27,34 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi("/swagger");
 }
 
-app.MapHealthChecks("/hc");
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json; charset=utf-8";
+
+        var response = new
+        {
+            status = report.Status.ToString(),
+            totalDuration = report.TotalDuration.TotalMilliseconds,
+            checks = report.Entries.Select(e => new
+            {
+                name = e.Key,
+                status = e.Value.Status.ToString(),
+                duration = e.Value.Duration.TotalMilliseconds,
+                description = e.Value.Description,
+                error = e.Value.Exception?.Message,
+            })
+        };
+
+        await context.Response.WriteAsync(
+            JsonSerializer.Serialize(response, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+            }));
+    }
+});
 
 app
     .MapCatalogEndpoints()
