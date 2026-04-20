836695224c1080f395e1ff03f0c51b80942581ce
Marcin99b <marcin.bigos99@gmail.com>
2026-04-20 19:00:37 +0200

    UpdateTrackingIndexCommand

--- src/Storygame.Web/Areas/Tracking/TrackingEndpoints.cs
+++ src/Storygame.Web/Areas/Tracking/TrackingEndpoints.cs
@@ -5,7 +5,8 @@ using Storygame.Tracking.Queries;
 
 namespace Storygame.Web.Areas.Tracking;
 
-public record StartTrackingRequest(int TotalLength);
+public record StartTrackingRequest(Guid LibraryBookId, int TotalLength);
+public record UpdateIndexRequest(int NewIndex);
 
 public static class TrackingEndpoints
 {
@@ -14,13 +15,17 @@ public static class TrackingEndpoints
         var group = app.MapGroup("/api/tracking").WithTags("Tracking").RequireAuthorization();
 
         group.MapGet("/", GetTrackings);
-        group.MapPost("/{libraryBookId:guid}", StartTracking);
+        group.MapPost("/", StartTracking);
+        group.MapPost("/{libraryBookId:guid}/index", UpdateIndex);
 
         return app;
     }
 
     public static Task<GetUserTrackingsQueryResult> GetTrackings(IDispatcher dispatcher, UserSession userSession)
         => dispatcher.QueryAsync<GetUserTrackingsQuery, GetUserTrackingsQueryResult>(new GetUserTrackingsQuery(userSession.UserId!.Value));
-    public static Task StartTracking(IDispatcher dispatcher, UserSession userSession, [FromRoute] Guid libraryBookId, [FromBody] StartTrackingRequest request)
-        => dispatcher.SendAsync(new StartTrackingBookCommand(libraryBookId, userSession.UserId!.Value, request.TotalLength));
+    public static Task StartTracking(IDispatcher dispatcher, UserSession userSession, [FromBody] StartTrackingRequest request)
+        => dispatcher.SendAsync(new StartTrackingBookCommand(request.LibraryBookId, userSession.UserId!.Value, request.TotalLength));
+
+    public static Task UpdateIndex(IDispatcher dispatcher, UserSession userSession, [FromRoute] Guid trackingId, [FromBody] UpdateIndexRequest request)
+        => dispatcher.SendAsync(new UpdateTrackingIndexCommand(trackingId, request.NewIndex));
 }
