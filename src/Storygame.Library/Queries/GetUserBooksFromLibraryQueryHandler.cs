63878046e7a77fe95ffed15782730720d52df414
Marcin99b <marcin.bigos99@gmail.com>
2026-04-20 18:56:07 +0200

    fix handler

--- src/Storygame.Library/Queries/GetUserBooksFromLibraryQueryHandler.cs
+++ src/Storygame.Library/Queries/GetUserBooksFromLibraryQueryHandler.cs
@@ -6,13 +6,13 @@ using System.Text;
 namespace Storygame.Library.Queries;
 
 public record GetUserBooksFromLibraryQuery(Guid UserId) : IQuery<GetUserBooksFromLibraryQueryResult>;
-public record GetUserBooksFromLibraryQueryResult;
+public record GetUserBooksFromLibraryQueryResult(IEnumerable<Book> Books);
 
 public class GetUserBooksFromLibraryQueryHandler(ILibraryRepository libraryRepository) : IQueryHandler<GetUserBooksFromLibraryQuery, GetUserBooksFromLibraryQueryResult>
 {
     public async Task<GetUserBooksFromLibraryQueryResult> HandleAsync(GetUserBooksFromLibraryQuery query)
     {
-        var books = libraryRepository.GetUserBooks(query.UserId);
-        return new GetUserBooksFromLibraryQueryResult();
+        var books = await libraryRepository.GetUserBooks(query.UserId);
+        return new GetUserBooksFromLibraryQueryResult(books);
     }
 }
