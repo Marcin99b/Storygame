5360de23c6826a93e05e81fa0f8a00155b8f1671
Marcin99b <marcin.bigos99@gmail.com>
2026-04-20 14:14:17 +0200

    GetUserTrackingsQueryHandler

--- src/Storygame.Storage/TrackingRepository.cs
+++ src/Storygame.Storage/TrackingRepository.cs
@@ -9,7 +9,9 @@ public class TrackingRepository(IMongoDatabase database) : ITrackingRepository
     private readonly IMongoCollection<Tracking.Tracking> tracking = database.GetCollection<Tracking.Tracking>(DbCollectionNames.Tracking);
 
     public void AddTracking(Tracking.Tracking tracking) => throw new NotImplementedException();
+
     public Task<bool> CheckIfBookIsAlreadyTracked(Guid libraryBookId) => throw new NotImplementedException();
+
     public async Task<IEnumerable<Tracking.Tracking>> GetUserTrackings(Guid userId)
     {
         return await tracking.AsQueryable().Where(x => x.UserId == userId).ToListAsync();
---
+++ src/Storygame.Tracking/Queries/GetUserTrackingsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Storygame.Cqrs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Tracking.Queries;
+
+public record GetUserTrackingsQuery(Guid UserId) : IQuery<GetUserTrackingsQueryResult>;
+
+public record GetUserTrackingsQueryResult(IEnumerable<Tracking> Trackings);
+
+public class GetUserTrackingsQueryHandler(ITrackingRepository trackingRepository) : IQueryHandler<GetUserTrackingsQuery, GetUserTrackingsQueryResult>
+{
+    public async Task<GetUserTrackingsQueryResult> HandleAsync(GetUserTrackingsQuery query)
+    {
+        var trackings = await trackingRepository.GetUserTrackings(query.UserId);
+        return new GetUserTrackingsQueryResult(trackings);
+    }
+}
