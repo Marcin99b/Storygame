f1714e923bb782325709a0a0e542e8591ac9a8a6
Marcin99b <marcin.bigos99@gmail.com>
2026-04-21 21:51:44 +0200

    traffic simulator

--- src/Storygame.Client/StorygameClient.cs
+++ src/Storygame.Client/StorygameClient.cs
@@ -8,8 +8,14 @@ public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
         Timeout = customTimeout ?? TimeSpan.FromSeconds(10)
     };
 
+    private static Uri CatalogPath = new Uri("/api/catalog");
+    private static Uri LibraryPath = new Uri("/api/library");
+    private static Uri TrackingPath = new Uri("/api/tracking");
+    private static Uri UsersPath = new Uri("/api/users");
+
     public async Task Login()
     {
+        var response = await client.PostAsync(new Uri(UsersPath, "/Login"), null);
 
     }
 }
---
+++ src/Storygame.TrafficSimulator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Storygame.Client;
+
+var url = new Uri("https://localhost:7121");
+var client = new StorygameClient(url);
+
+await client.Login();
+
+await Task.Delay(-1);
