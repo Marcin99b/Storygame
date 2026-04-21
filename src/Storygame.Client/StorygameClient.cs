a0c3563303729c665d4aab136c96da483bd735c1
Marcin99b <marcin.bigos99@gmail.com>
2026-04-21 21:37:59 +0200

    StorygameClient

---
+++ src/Storygame.Client/StorygameClient.cs
@@ -0,0 +1,15 @@
+﻿namespace Storygame.Client;
+
+public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
+{
+    private readonly HttpClient client = new HttpClient()
+    {
+        BaseAddress = new UriBuilder(address.Scheme, address.Host, address.Port).Uri,
+        Timeout = customTimeout ?? TimeSpan.FromSeconds(10)
+    };
+
+    public async Task Login()
+    {
+
+    }
+}
