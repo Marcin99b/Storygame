9ca67ecaeefb29e30a920bf8d41ad9ce497f368e
Marcin99b <marcin.bigos99@gmail.com>
2026-04-21 15:00:30 +0200

    events in dispatcher

--- src/Storygame.Cqrs/Dispatcher.cs
+++ src/Storygame.Cqrs/Dispatcher.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Storygame.Logging;
 
 namespace Storygame.Cqrs;
 
-public sealed class Dispatcher(IServiceProvider serviceProvider) : IDispatcher
+public sealed class Dispatcher(IServiceProvider serviceProvider, ILogger<Dispatcher> logger) : IDispatcher
 {
     public Task<TResult> QueryAsync<TQuery, TResult>(TQuery query) 
         where TQuery : IQuery<TResult>
     {
         using var scope = serviceProvider.CreateScope();
-        var handler = scope.ServiceProvider.GetService<IQueryHandler<TQuery, TResult>>()!;
+        var handler = scope.ServiceProvider.GetRequiredService<IQueryHandler<TQuery, TResult>>();
+
+        logger.ExecutingQuery(typeof(TQuery).Name);
+
         return handler.HandleAsync(query);
     }
 
@@ -16,7 +21,10 @@ public sealed class Dispatcher(IServiceProvider serviceProvider) : IDispatcher
         where TCommand : ICommand
     {
         using var scope = serviceProvider.CreateScope();
-        var handler = scope.ServiceProvider.GetService<ICommandHandler<TCommand>>()!;
+        var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+        logger.ExecutingCommand(typeof(TCommand).Name);
+
         return handler.HandleAsync(command);
     }
 
@@ -25,6 +33,9 @@ public sealed class Dispatcher(IServiceProvider serviceProvider) : IDispatcher
     {
         using var scope = serviceProvider.CreateScope();
         var handlers = scope.ServiceProvider.GetServices<IEventHandler<TEvent>>()!;
+
+        logger.PublishingEvent(typeof(TEvent).Name);
+
         var tasks = handlers.Select(x => x.HandleAsync(@event));
         return Task.WhenAll(tasks);
     }
