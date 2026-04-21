211dbef66a6a7eaf7b5c8ce819fb18ecc1e65b55
Marcin99b <marcin.bigos99@gmail.com>
2026-04-21 14:51:22 +0200

    LoggerExtensions

---
+++ src/Storygame.Logging/LoggerEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Storygame.Logging;
+
+public enum LoggerEvent
+{
+    ExecutingQuery = 1,
+    ExecutingCommand = 2,
+    PublishingEvent = 3,
+}
---
+++ src/Storygame.Logging/LoggerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Storygame.Logging;
+
+public static partial class LoggerExtensions
+{
+    [LoggerMessage(EventId = (int)LoggerEvent.ExecutingQuery, EventName = nameof(LoggerEvent.ExecutingQuery), Level = LogLevel.Information, Message = "Executing query {queryName}", SkipEnabledCheck = false)]
+    public static partial void ExecutingQuery(this ILogger logger, string queryName);
+
+    [LoggerMessage(EventId = (int)LoggerEvent.ExecutingCommand, EventName = nameof(LoggerEvent.ExecutingCommand), Level = LogLevel.Information, Message = "Executing command {commandName}", SkipEnabledCheck = false)]
+    public static partial void ExecutingCommand(this ILogger logger, string commandName);
+
+    [LoggerMessage(EventId = (int)LoggerEvent.PublishingEvent, EventName = nameof(LoggerEvent.PublishingEvent), Level = LogLevel.Information, Message = "Publishing event {eventName}", SkipEnabledCheck = false)]
+    public static partial void PublishingEvent(this ILogger logger, string eventName);
+}
