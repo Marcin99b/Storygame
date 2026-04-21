847c3c8cf12aa14a191f788b1cc05920260a3c19
Marcin99b <marcin.bigos99@gmail.com>
2026-04-22 00:05:31 +0200

    randoms in simulation scenario

--- src/Storygame.TrafficSimulator/Program.cs
+++ src/Storygame.TrafficSimulator/Program.cs
@@ -2,36 +2,49 @@
 using Storygame.Contracts.WebApi;
 
 var url = new Uri("https://localhost:7121");
-var client = new StorygameClient(url);
 
-await client.Login();
-var me = await client.Me();
-var catalog = await client.GetCatalog();
-var book1 = catalog.Books[0];
+var tasks = Enumerable.Range(0, 30).Select(x => SimpleScenario());
+await Task.WhenAll(tasks);
 
-await client.AddToLibrary(new (
-    book1.Id, 
-    book1.ImageId, 
-    book1.Title, 
-    book1.Description,
-    MediaTypeDto.Audiobook, 
-    book1.AudiobookFields.TotalMinutes));
+async Task SimpleScenario()
+{
+    var client = new StorygameClient(url);
+    await client.Login();
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(1, 3)));
+    var me = await client.Me();
+    var catalog = await client.GetCatalog();
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(5, 10)));
+    var book1 = catalog.Books[0];
 
-var library = await client.GetLibrary();
+    await client.AddToLibrary(new(
+        book1.Id,
+        book1.ImageId,
+        book1.Title,
+        book1.Description,
+        MediaTypeDto.Audiobook,
+        book1.AudiobookFields.TotalMinutes));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(1, 5)));
 
-var libraryBook1 = library.Books[0];
+    var library = await client.GetLibrary();
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(2, 4)));
 
-await client.StartTracking(new (libraryBook1.Id, libraryBook1.Length));
+    var libraryBook1 = library.Books[0];
 
-var trackings = await client.GetTrackings();
-var tracking1 = trackings.Trackings[0];
+    await client.StartTracking(new(libraryBook1.Id, libraryBook1.Length));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(15, 60)));
+    var trackings = await client.GetTrackings();
+    var tracking1 = trackings.Trackings[0];
 
-await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(5));
-await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(15));
-await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(25));
-await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(30));
-await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(50));
+    await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(5));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(15, 60)));
+    await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(15));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(15, 60)));
+    await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(25));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(15, 60)));
+    await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(30));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(15, 60)));
+    await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(50));
+    await Task.Delay(TimeSpan.FromSeconds(new Random().Next(15, 60)));
 
-var updatedTrackings = await client.GetTrackings();
-
-await Task.Delay(-1);
+    var updatedTrackings = await client.GetTrackings();
+}
