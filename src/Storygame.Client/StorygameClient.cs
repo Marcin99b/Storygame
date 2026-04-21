cc87f3e897fae04e9613fd73132af2102542c8fe
Marcin99b <marcin.bigos99@gmail.com>
2026-04-21 23:32:11 +0200

    fix client enum json

--- src/Storygame.Client/StorygameClient.cs
+++ src/Storygame.Client/StorygameClient.cs
@@ -2,6 +2,8 @@
 using Storygame.Contracts.WebApi;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Storygame.Client;
 
@@ -12,24 +14,20 @@ public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
     private const string TrackingPath = "/api/tracking";
     private const string UsersPath = "/api/users";
 
+    private static readonly JsonSerializerOptions jsonOptions = CreateJsonOptions();
+
     private readonly HttpClient client = new HttpClient()
     {
         BaseAddress = new UriBuilder(address.Scheme, address.Host, address.Port).Uri,
-        Timeout = customTimeout ?? TimeSpan.FromSeconds(10)
+        Timeout = customTimeout ?? TimeSpan.FromSeconds(10),
     };
 
     public async Task Login()
     {
         var url = UsersPath + "/Login";
         var response = await client.PostAsync(url, null);
-        var cookie = response.Headers.GetValues("Set-Cookie").Single();
-
-        if (client.DefaultRequestHeaders.Contains(HeaderNames.Cookie))
-        {
-            client.DefaultRequestHeaders.Remove(HeaderNames.Cookie);
-        }
-        
-        client.DefaultRequestHeaders.Add(HeaderNames.Cookie, cookie);
+        response.EnsureSuccessStatusCode();
+        TryUpdateCookie(response);
     }
 
     public async Task<MeResponse> Me()
@@ -37,7 +35,8 @@ public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
         var url = UsersPath + "/Me";
         var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
-        return (await response.Content.ReadFromJsonAsync<MeResponse>())!;
+        TryUpdateCookie(response);
+        return (await response.Content.ReadFromJsonAsync<MeResponse>(jsonOptions))!;
     }
 
     public async Task<GetCatalogResponse> GetCatalog()
@@ -45,14 +44,16 @@ public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
         var url = CatalogPath;
         var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
-        return (await response.Content.ReadFromJsonAsync<GetCatalogResponse>())!;
+        TryUpdateCookie(response);
+        return (await response.Content.ReadFromJsonAsync<GetCatalogResponse>(jsonOptions))!;
     }
 
     public async Task AddToLibrary(AddToLibraryRequest request)
     {
         var url = LibraryPath;
-        var response = await client.PostAsJsonAsync(url, request);
+        var response = await client.PostAsJsonAsync(url, request, jsonOptions);
         response.EnsureSuccessStatusCode();
+        TryUpdateCookie(response);
     }
 
     public async Task<GetLibraryResponse> GetLibrary()
@@ -60,14 +61,16 @@ public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
         var url = LibraryPath;
         var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
-        return (await response.Content.ReadFromJsonAsync<GetLibraryResponse>())!;
+        TryUpdateCookie(response);
+        return (await response.Content.ReadFromJsonAsync<GetLibraryResponse>(jsonOptions))!;
     }
 
     public async Task StartTracking(StartTrackingRequest request)
     {
         var url = TrackingPath;
-        var response = await client.PostAsJsonAsync(url, request);
+        var response = await client.PostAsJsonAsync(url, request, jsonOptions);
         response.EnsureSuccessStatusCode();
+        TryUpdateCookie(response);
     }
 
     public async Task<GetTrackingsResponse> GetTrackings()
@@ -75,13 +78,42 @@ public class StorygameClient(Uri address, TimeSpan? customTimeout = null)
         var url = TrackingPath;
         var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
-        return (await response.Content.ReadFromJsonAsync<GetTrackingsResponse>())!;
+        TryUpdateCookie(response);
+        return (await response.Content.ReadFromJsonAsync<GetTrackingsResponse>(jsonOptions))!;
     }
 
     public async Task UpdateIndex(Guid trackingId, UpdateIndexRequest request)
     {
         var url = TrackingPath + $"/{trackingId}/index";
-        var response = await client.PostAsJsonAsync(url, request);
+        var response = await client.PostAsJsonAsync(url, request, jsonOptions);
         response.EnsureSuccessStatusCode();
+        TryUpdateCookie(response);
+    }
+
+    private void TryUpdateCookie(HttpResponseMessage response)
+    {
+        if (!response.Headers.TryGetValues("Set-Cookie", out var cookies))
+        {
+            return;
+        }
+
+        var cookie = cookies.Single();
+
+        if (client.DefaultRequestHeaders.Contains(HeaderNames.Cookie))
+        {
+            client.DefaultRequestHeaders.Remove(HeaderNames.Cookie);
+        }
+
+        client.DefaultRequestHeaders.Add(HeaderNames.Cookie, cookie);
+    }
+
+    private static JsonSerializerOptions CreateJsonOptions()
+    {
+        var options = new JsonSerializerOptions();
+
+        options.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+        options.PropertyNameCaseInsensitive = true;
+
+        return options;
     }
 }
--- src/Storygame.TrafficSimulator/Program.cs
+++ src/Storygame.TrafficSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Storygame.Client;
+using Storygame.Contracts.WebApi;
 
 var url = new Uri("https://localhost:7121");
 var client = new StorygameClient(url);
@@ -6,5 +7,31 @@ var client = new StorygameClient(url);
 await client.Login();
 var me = await client.Me();
 var catalog = await client.GetCatalog();
+var book1 = catalog.Books[0];
+
+await client.AddToLibrary(new AddToLibraryRequest(
+    book1.Id, 
+    book1.ImageId, 
+    book1.Title, 
+    book1.Description,
+    MediaTypeDto.Audiobook, 
+    book1.AudiobookFields.TotalMinutes));
+
+var library = await client.GetLibrary();
+
+var libraryBook1 = library.Books[0];
+
+await client.StartTracking(new StartTrackingRequest(libraryBook1.Id, libraryBook1.Length));
+
+var trackings = await client.GetTrackings();
+var tracking1 = trackings.Trackings[0];
+
+await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(5));
+await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(15));
+await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(25));
+await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(30));
+await client.UpdateIndex(tracking1.Id, new UpdateIndexRequest(50));
+
+var updatedTrackings = await client.GetTrackings();
 
 await Task.Delay(-1);
