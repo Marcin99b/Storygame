9bf338ca52bbe7b3fe3302bf3f5037c01a93967e
Marcin99b <marcin.bigos99@gmail.com>
2026-04-26 13:28:21 +0200

    WebAppFactory

---
+++ src/Storygame.Tests.Integration/WebAppFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Storygame.Tests.Integration;
+
+public static class WebAppFactory
+{
+    public static HttpClient CreateHttpClient(Action<IServiceCollection>? customServices = null)
+    {
+        var factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder => 
+            {
+                builder.ConfigureServices(services => 
+                {
+                    customServices?.Invoke(services);
+                });
+            });
+
+        return factory.CreateClient();
+    }
+}
