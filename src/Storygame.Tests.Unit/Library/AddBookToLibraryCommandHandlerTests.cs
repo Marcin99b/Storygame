8ed9c710cf7ccb9552ef09241c6715acdf3168ea
Marcin99b <marcin.bigos99@gmail.com>
2026-05-08 19:05:07 +0200

    save

---
+++ src/Storygame.Tests.Unit/Library/AddBookToLibraryCommandHandlerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storygame.Tests.Unit.Library;
+
+[TestFixture]
+public class AddBookToLibraryCommandHandlerTests
+{
+}
